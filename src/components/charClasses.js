bd14e3c383e14e98e7bac9c034fa8570717839d6
Jacek Stefanski <jacekstefanski@Jaceks-iMac.local>
2015-09-28 16:58:56 -0400

    changes
    classes, etc.

--- src/components/charClasses.js
+++ src/components/charClasses.js
@@ -2,135 +2,153 @@
 
 export default {
   'A': {
-    wrapper: 'metrics_cont Y',
+    wrapper: 'metrics_h width_six',
     inner: ['A sub_stroke origin_left color'],
   },
   'B': {
-    wrapper: 'metrics_cont med',
+    wrapper: 'metrics_h width_five',
     inner: ['B color', 'B_counter'],
   },
   'C': {
-    wrapper: 'metrics_cont mid over-y',
+    wrapper: 'metrics_h auto-y width_three',
     inner: ['C color'],
   },
   'D': {
-    wrapper: 'metrics_cont way over-y',
+    wrapper: 'metrics_h auto-y width_four',
     inner: [
       'A_I third_stroke color',
       'D origin_left color',
     ],
   },
   'E': {
-    wrapper: 'metrics_cont low',
+    wrapper: 'metrics_h width_two',
     inner: [
       'E sec_stroke',
       'hor_stroke',
     ],
   },
   'F': {
-    wrapper: 'metrics_cont low',
+    wrapper: 'metrics_h width_two',
     inner: ['F sec_stroke'],
   },
   'G': {
-    wrapper: 'metrics_cont Y',
+    wrapper: 'metrics_h width_six',
     inner: [
       'G_count',
       'G color',
     ],
   },
   'H': {
-    wrapper: 'metrics_cont med',
+    wrapper: 'metrics_h width_five',
     inner: [
       'H third_stroke',
     ],
   },
   'I': {
-    wrapper: 'metrics_cont est',
+    wrapper: 'metrics_h width_one',
     inner: ['I color'],
   },
   'J': {
-    wrapper: 'auto_over metrics_cont2 est',
+    wrapper: 'auto_over metrics_h width_one',
     inner: ['J color'],
   },
   'K': {
-    wrapper: 'metrics_cont way',
+    wrapper: 'metrics_h width_four',
     inner: ['K color'],
   },
   'L': {
-    wrapper: 'metrics_cont low',
+    wrapper: 'metrics_h width_two',
     inner: ['L sec_stroke'],
   },
   'M': {
-    wrapper: 'metrics_cont full',
+    wrapper: 'metrics_h width_seven',
     inner: ['M origin_left', 'M_II origin_right right'],
   },
   'N': {
-    wrapper: 'metrics_cont over-y way',
+    wrapper: 'metrics_h over-y width_four',
     inner: ['N main_stroke color'],
   },
   'O': {
-    wrapper: 'metrics_cont Y',
+    wrapper: 'metrics_h width_six',
     inner: ['O color'],
   },
   'P': {
-    wrapper: 'metrics_cont med',
+    wrapper: 'metrics_h width_five',
     inner: ['P third_stroke color'],
   },
   'Q': {
-    wrapper: 'metrics_cont Y',
+    wrapper: 'metrics_h width_six',
     inner: ['Q color'],
   },
   'R': {
-    wrapper: 'metrics_cont way',
+    wrapper: 'metrics_h width_four',
     inner: [
       'A_I main_stroke color',
       'R_bowl color',
     ],
   },
   'S': {
-    wrapper: 'auto_over metrics_cont2 low',
+    wrapper: 'auto_over metrics_h width_two',
     inner: ['S_top', 'S_bottom '],
   },
   'T': {
-    wrapper: 'metrics_cont mid',
+    wrapper: 'metrics_h width_three',
     inner: ['T sec_stroke'],
   },
   'U': {
-    wrapper: 'metrics_cont med',
+    wrapper: 'metrics_h width_five',
     inner: ['U'],
   },
   'V': {
-    wrapper: 'metrics_cont Y',
+    wrapper: 'metrics_h width_six',
     inner: [
       'A_I main_stroke ten_pos origin_top_l color',
       'A_I main_stroke ten_neg origin_top_r right color',
     ],
   },
   'W': {
-    wrapper: 'metrics_cont full_ex',
+    wrapper: 'metrics_h width_eight',
     inner: [
       'W origin_top_l color',
       'W_II origin_top_l right color',
     ],
   },
   'X': {
-    wrapper: 'metrics_cont Y',
+    wrapper: 'metrics_h width_six',
     inner: [
       'X_I origin_left color',
     ],
   },
   'Y': {
-    wrapper: 'metrics_cont Y',
+    wrapper: 'metrics_h width_six',
     inner: [
       'Y_I third_stroke color',
     ],
   },
   'Z': {
-    wrapper: 'metrics_cont over-y way',
+    wrapper: 'metrics_h auto-y width_four',
     inner: ['Z origin_left third_stroke color'],
   },
-  ' ': {
-    wrapper: 'metrics_cont full_ex',
+  '.': {
+    wrapper: 'metrics_h width_six',
+    inner: [
+      'PER third_stroke color',
+    ],
+  },
+   '!': {
+    wrapper: 'metrics_h width_one',
+    inner: [
+      'EXC third_stroke color',
+    ],
+  },
+   '?': {
+    wrapper: 'metrics_h width_three',
+    inner: [
+      'QUE third_stroke color',
+      'QUE_II color',
+    ],
+  },  ' ': {
+    wrapper: 'metrics_h width_one',
     inner: [],
   },
 };
